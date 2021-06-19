e473fa33c40078700814c3b232f1355092694542
Amit-proj <amitsalman80@gmail.com>
2021-06-19 19:06:12 +0300

    adding splash to the project and class for breaks

---
+++ app/src/main/java/syntax/org/il/gameproject/SplashActivity.java
@@ -0,0 +1,43 @@
+package syntax.org.il.gameproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3100;
+
+    //variables
+    Animation upAnim;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.splash);
+
+            //Animations
+            upAnim = AnimationUtils.loadAnimation(this,R.anim.up_anim);
+            image = findViewById(R.id.splash_imageView);
+            image.setAnimation(upAnim);
+
+            //Move to another activity
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                 Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+             }
+         },SPLASH_SCREEN);
+
+    }
+}
