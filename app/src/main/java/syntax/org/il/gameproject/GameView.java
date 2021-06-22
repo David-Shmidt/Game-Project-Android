9d5166a85d185aed61ecf0c8bbccdd316163034c
David-Shmidt <davidshmidt@gmail.com>
2021-06-22 20:09:41 +0300

    Game-Project David: Added Bricks creation Dynamically and added bricks colission with ball David:Added the ability for blocks to disappear when getting hit

--- app/src/main/java/syntax/org/il/gameproject/GameView.java
+++ app/src/main/java/syntax/org/il/gameproject/GameView.java
@@ -29,14 +29,16 @@ import androidx.annotation.Nullable;
 public class GameView extends TextView {
 
     float StartX ,startY , endX , endY;
-Paint paint;
-Paint transperent = new Paint(Paint.ANTI_ALIAS_FLAG);
-Rect gameRect = new Rect();
-Rect deletRect = new Rect();
-RectF deletRectF  =new RectF();
-boolean toDelete = false;
-boolean toDeleteF = false;
-private Bitmap life[] = new Bitmap[2];
+    Paint paint;
+    Paint transperent = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Rect gameRect = new Rect();
+    Rect deletRect = new Rect();
+    RectF deletRectF  =new RectF();
+    boolean toDelete = false;
+    boolean toDeleteF = false;
+
+    int scale = (int)getResources().getDisplayMetrics().density;
+
 
     int Row, Colum;
     Rect[][] matrix;
@@ -54,16 +56,41 @@ private Bitmap life[] = new Bitmap[2];
         paint.setColor(Color.RED);
         paint.setColor(getResources().getColor(R.color.teal_200));
         //paint.setColor(getResources().getDrawable(R.drawable.platform));
-       // paint.setColor( Color.rgb(23, 56, 10) );
-      //  paint.setStrokeWidth(30);
+        // paint.setColor( Color.rgb(23, 56, 10) );
+        //  paint.setStrokeWidth(30);
         paint.setStyle(Paint.Style.FILL);
         transperent.setColor(Color.TRANSPARENT);
         transperent.setStyle(Paint.Style.FILL);
 
         //Life
-        //backroundImage = BitmapFactory.decodeResource(getResources(),R.drawable.backround);
-       // life[0] = BitmapFactory.decodeResource(getResources(), R.drawable.heart);
-        //life[1] = BitmapFactory.decodeResource(getResources(),R.drawable.empty_heart);
+        heart.moveTo(25f * scale,5f* scale);
+        heart.lineTo(35f* scale,10f* scale);
+        heart.lineTo(45f* scale,5f* scale);
+        heart.lineTo(55f* scale,10f* scale);
+        heart.lineTo(35f* scale,25f* scale);
+        heart.lineTo(15f* scale,10f* scale);
+        heart.lineTo(25f* scale,5f* scale);
+        heart.close();
+
+
+        heart.moveTo(75f* scale,5f* scale);
+        heart.lineTo(85f* scale,10f* scale);
+        heart.lineTo(95f* scale,5f* scale);
+        heart.lineTo(105f* scale,10f* scale);
+        heart.lineTo(85f* scale,25f* scale);
+        heart.lineTo(65f* scale,10f* scale);
+        heart.lineTo(75f* scale,5f* scale);
+        heart.close();
+
+        heart.moveTo(125f* scale,5f* scale);
+        heart.lineTo(135f* scale,10f* scale);
+        heart.lineTo(145f* scale,5f* scale);
+        heart.lineTo(155f* scale,10f* scale);
+        heart.lineTo(135f* scale,25f* scale);
+        heart.lineTo(40f* scale,10f* scale);
+        heart.lineTo(125f* scale,5f* scale);
+        heart.close();
+
 
     }
 
@@ -72,7 +99,7 @@ private Bitmap life[] = new Bitmap[2];
         super.onDraw(canvas);
 
         if(!toDelete){
-        //canvas.drawRect(gameRect,paint);
+            //canvas.drawRect(gameRect,paint);
             for(int i = 0; i<Row ; i++){
                 for(int j=0;j<Colum;j++){
                     if(matrix[i][j] != deletRect) {
@@ -88,13 +115,6 @@ private Bitmap life[] = new Bitmap[2];
         }
 
         canvas.drawPath(heart , paint);
-        //life
-        /*canvas.drawBitmap(life[0],10,10,null);
-        canvas.drawBitmap(life[0],20,10,null);
-        canvas.drawBitmap(life[0],30,10,null);*/
-
-
-
     }
 
 
@@ -105,16 +125,16 @@ private Bitmap life[] = new Bitmap[2];
         Colum = colum;
         bricks = new Rect[Row*Colum];
         for(int i = 0; i<Row;i++){
-            heightCahnge += 110;
+            heightCahnge += 35;
             widthChange = 0;
             for(int j = 0; j<Colum; j++){
                 matrix[i][j] = new Rect();
-                matrix[i][j].set(200+widthChange ,100 + heightCahnge ,400 + widthChange, 200 + heightCahnge );
+                matrix[i][j].set(scale*(50+widthChange) , scale*(30 + heightCahnge ),  scale*(100 + widthChange),  scale*(60 + heightCahnge) );
                 bricks[bricksCount] = matrix[i][j];
                 bricksCount++;
                 //bricks[bricksCount] = new Rect(200+widthChange ,100 + heightCahnge ,400 + widthChange, 200 + heightCahnge);
                 //canvas.drawRect(matrix[i][j] , paint);
-                widthChange +=210;
+                widthChange +=55;
             }
         }
         invalidate();
